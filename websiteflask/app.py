b9998af81c8c9f44d3e007e879184d8226c376dc
jjee <najed321@gmail.com>
2022-11-10 19:28:55 +0300

    'j'

--- websiteflask/app.py
+++ websiteflask/app.py
@@ -2,7 +2,7 @@ from email import message
 from multiprocessing import connection
 import sqlite3
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,jsonify
 import os
 
 currdir=os.path.dirname(os.path.abspath(__file__))
@@ -45,7 +45,39 @@ def save():
         cursor.execute(q1)
         connection.commit()
         return render_template('neww.html')
-    return render_template('neww.html',message="")
+    return render_template('neww.html')
+
+@app.route('/open',methods=["GET"])
+def open():
+    if request.method =='GET':
+        connection=sqlite3.connect("./featureDiagram.db")
+        cursor=connection.cursor()
+        q2="SELECT * FROM FeatureDiagarm"
+        res=cursor.execute(q2)
+        res=res.fetchall()
+        #print(res["ID"])
+        return render_template('open.html',diagrams=res)
+    return render_template('neww.html')
+@app.route('/drow',methods=["POST"])
+def drow():
+    if request.method =='POST':
+        jose=request.form.get('TEx')
+        print(jose)
+        connection=sqlite3.connect("./featureDiagram.db")
+        cursor=connection.cursor()
+        q2="SELECT JSON_VALUE FROM FeatureDiagarm where ID ={N}".format(N=jose)
+        res=cursor.execute(q2)
+
+        res=res.fetchall()
+        cont={}
+        rel=[]
+       # for ress in res :
+        print(res[0][0])
+        #cont={'class':res[0],'nodeDataArray':res[1],'linkDataArray':res[2]}
+        #rel.append(cont)
+        
+        #print(jsonify(rel))
+        return render_template('neww.html',diagrams=res[0][0])
 
 @app.route('/team', endpoint ='team')
 def team():
--- websiteflask/database.py
+++ websiteflask/database.py
@@ -1,32 +1,41 @@
 import sqlite3  
 
+
+
 conn = sqlite3.connect('featureDiagram.db')
 
 print ("Opened database successfully")
+q2="SELECT * FROM FeatureDiagarm"
+cursor=conn.cursor()
+res=cursor.execute(q2)
+res=res.fetchall()
+print(res[0][1])
+conn.close()
+'''
 
-conn.execute('''CREATE TABLE if not exists FeatureDiagarm
+conn.execute(CREATE TABLE if not exists FeatureDiagarm
 
          (ID            INT     NOT NULL,
 
          JSON_VALUE           TEXT    NOT NULL
 
-         );''')
+         );)
 
 print ("Table created successfully")
 
 conn.close()
 
-conn = sqlite3.connect('featureDiagram.db')
+conn = sqlite3.connect('./featureDiagram.db')
 
 print ("Opened database successfully")
 
-'''
+
 
 conn.execute("INSERT INTO FeatureDiagarm (ID,JSON_VALUE) 
 
       VALUES (1, ' Paul  - - - - -- ' )")
 
-'''
+
 
 
 conn.commit()
@@ -55,4 +64,5 @@ for row in cursor:
 
 print ("Operation done successfully")
 
-conn.close()
+
+'''
